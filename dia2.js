8a6debef114a7eb24df0d5528b773c814d9516a9
ArturoJARH <arturohermosillo02@gmail.com>
2025-12-19 16:30:27 -0600

    Add files via upload

---
+++ dia2.js
@@ -0,0 +1,66 @@
+// Tipos principales:
+let nombre = "Arturo";
+let edad = 18;
+let ciudad = "La Chona";
+
+console.log(nombre);
+console.log(edad);
+console.log(ciudad);
+
+console.log("Hola", nombre);
+
+// Tipos de datos
+let texto = "Hola";       // string
+let numero = 10;         // number
+let verdadero = true;    // boolean
+let nada = null;         // null
+let indefinido;          // undefined
+
+console.log("Tipos de datos:");
+console.log(texto);
+console.log(numero);
+console.log(verdadero);
+console.log(nada);
+console.log(indefinido);
+
+// Ejercicio
+console.log("Ejercicio tipos de datos:");
+let comida = "Tacos";
+let precio = 50;
+let hay = true;
+
+console.log(comida, precio, hay);
+
+
+// Operaciones básicas
+let a = 10;
+let b = 5;
+console.log("Operaciones básicas:"); 
+console.log(a + b); // suma
+console.log(a - b); // resta
+console.log(a * b); // multiplicación
+console.log(a / b); // división
+
+// Ejercicio
+console.log("Ejercicio operaciones básicas:");
+console.log(edad + 5);
+console.log(100 / 2);
+console.log(10 * 3);
+
+/*
+Mini reto del día (30 min)
+
+💡 Reto
+Haz un programa que muestre:
+
+Tu nombre
+
+Tu edad
+
+Cuántos años tendrás en 5 años*/
+
+// Mini reto del día
+console.log("Mini reto del día:");
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("Edad en 5 años:", edad + 5);
