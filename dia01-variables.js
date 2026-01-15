a9c5a8aba822340c54c70490545d77358751eaf5
José Arturo Romo Hermosillo <arturohermosillo02@gmail.com>
2026-01-15 02:17:42 -0600

    Update and rename dia1.js to dia01-variables.js

---
+++ dia01-variables.js
@@ -0,0 +1,30 @@
+/*
+==================================================
+ Día 01 - Variables en JavaScript
+ Autor: José Arturo Romo Hermosillo
+ Descripción:
+ Introducción a variables, tipos de datos básicos
+ y uso de console.log.
+==================================================
+*/
+
+console.log("\n|>>>>>>>>>>>>>>>>>>>>>>>>>>>>·<<<<<<<<<<<<<<<<<<<<<<<<<<<<|\n");
+
+console.log("               Dia 01 - JavaScript Avanzado              ");
+console.log("           De: José Arturo Romo Hermosillo              ");
+
+console.log("\n|>>>>>>>>>>>>>>>>>>>>>>>>>>>>·<<<<<<<<<<<<<<<<<<<<<<<<<<<<|\n");
+
+console.log("Hola, este es mi día 1 con JavaScript");
+
+let nombre = "Arturo";
+let edad = 18;
+
+console.log("Mi nombre es " + nombre);
+console.log("Tengo " + edad + " años");
+
+console.log("\n|>>>>>>>>>>>>>>>>>>>>>>>>>>>>·<<<<<<<<<<<<<<<<<<<<<<<<<<<<|\n");
+
+console.log("                    Fin del día 01                         ");
+
+console.log("\n|>>>>>>>>>>>>>>>>>>>>>>>>>>>>·<<<<<<<<<<<<<<<<<<<<<<<<<<<<|\n");
--- dia1.js
+++
@@ -1,6 +0,0 @@
-console.log("Hola, este es mi día 1 con JavaScript");
-let nombre = "Arturo";
-let edad = 18;
-
-console.log("Minombre es " + nombre);
-console.log("Tengo " + edad + "años");
